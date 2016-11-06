0aeb25aaa5a13e43d4e60a96ca4be441506be4e6
abatar1 <evgenyhalzov@gmail.com>
2016-11-06 16:12:25 +0600

    1 task

---
+++ Week7.Test/UnitTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace Week7.Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TimerTest()
+        {
+            var timer = new Timer();
+            using (timer.Start())
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+            Assert.AreEqual(timer.ElapsedSeconds, 1);
+
+            using (timer.Continue())
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }
+            Assert.AreEqual(timer.ElapsedSeconds, 3);
+
+            using (timer.Start())
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(3));
+            }
+            Assert.AreEqual(timer.ElapsedSeconds, 3);
+        }
+    }
+}
---
+++ Week7/Timer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Week7
+{
+    public class Timer : IDisposable 
+    {
+        private Stopwatch stopWatch;
+
+        public long ElapsedMilliseconds
+        {
+            get
+            {
+                return stopWatch.ElapsedMilliseconds;
+            }
+        }    
+        
+        public long ElapsedSeconds
+        {
+            get
+            {
+                return (long)Math.Round(stopWatch.ElapsedMilliseconds / 1000.0);
+            }
+        }
+
+        public Timer Start()
+        {
+            stopWatch = new Stopwatch();
+            stopWatch.Start();
+            return this;
+        }
+
+        public Timer Continue()
+        {
+            stopWatch.Start();
+            return this;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    stopWatch.Stop();                   
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
