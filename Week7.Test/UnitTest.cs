6804ba92f1658952e60fe247d2656552ab934a21
abatar1 <evgenyhalzov@gmail.com>
2016-11-06 17:43:49 +0600

    2 task

--- Week7.Test/UnitTest.cs
+++ Week7.Test/UnitTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
+using System.Drawing;
 
 namespace Week7.Test
 {
@@ -28,6 +29,37 @@ namespace Week7.Test
                 Thread.Sleep(TimeSpan.FromSeconds(3));
             }
             Assert.AreEqual(timer.ElapsedSeconds, 3);
+        }
+
+        public void BitmapTest()
+        {
+            string filename = "";
+            var bitmap = (Bitmap)Image.FromFile(filename);
+
+            var timer1 = new Timer();
+            using (timer1.Start())
+            {
+                using (var bitmapEditor = new BitmapEditor(bitmap))
+                {
+                    for (int i = 0; i < bitmapEditor.Height; i++)
+                        for (int j = 0; j < bitmapEditor.Width; j++)
+                        {
+                            bitmapEditor.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                        }
+                }
+            }
+
+            var timer2 = new Timer();
+            using (timer2.Start())
+            {
+                for (int i = 0; i < bitmap.Height; i++)
+                    for (int j = 0; j < bitmap.Width; j++)
+                    {
+                        bitmap.SetPixel(i, j, Color.FromArgb(255, 255, 255));
+                    }
+            }
+
+            Assert.IsTrue(timer1.ElapsedMilliseconds - timer2.ElapsedMilliseconds < 0);
         }
     }
 }
---
+++ Week7/BitmapEditor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Week7
+{
+    public class BitmapEditor : IDisposable
+    {
+        private BitmapData bitmapData;
+        private Bitmap bitmap;
+
+        private int bytes;
+        private byte[] rgbValues;
+
+        public int Width { get { return bitmap.Width; } }
+        public int Height { get { return bitmap.Height; } }
+
+        public BitmapEditor(Bitmap _bitmap)
+        {
+            bitmap = new Bitmap(_bitmap);
+            Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            
+            bitmapData = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
+
+            bytes = Math.Abs(bitmapData.Stride) * bitmap.Height;
+            rgbValues = new byte[bytes];
+
+            Marshal.Copy(bitmapData.Scan0, rgbValues, 0, bytes);
+        }
+
+        public unsafe void SetPixel(int x, int y, Color color)
+        {
+            if (x > bitmap.Height || y > bitmap.Width)
+                throw new IndexOutOfRangeException();
+
+            switch (bitmapData.PixelFormat)
+            {
+                case PixelFormat.Format24bppRgb:
+                    byte* pos = (byte*)bitmapData.Scan0 + x * 3 + bitmapData.Stride * y;
+                    int rawColor = color.ToArgb();
+                    pos[0] = (byte)(rawColor & 0xFF);
+                    pos[1] = (byte)((rawColor >> 8) & 0xFF);
+                    pos[2] = (byte)((rawColor >> 16) & 0xFF);
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Marshal.Copy(rgbValues, 0, bitmapData.Scan0, bytes);
+                    bitmap.UnlockBits(bitmapData);
+                }
+            
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
