dc74bfc77b0e3de131e4033c718a7493799ddce4
abatar1 <evgenyhalzov@gmail.com>
2016-11-09 14:36:45 +0600

    2 task 1.4 update

--- Week7/BitmapEditor.cs
+++ Week7/BitmapEditor.cs
@@ -28,18 +28,22 @@ namespace Week7
                 throw new IndexOutOfRangeException();
 
             var ptr = bitmapData.Scan0;
-            var offset = bitmapData.Stride;
-            var cPtr = new IntPtr(color.ToArgb());
+            var offset = bitmapData.Stride * y;
 
             switch (bitmapData.PixelFormat)
             {
                 //here comes cases with pixel formats
-                //case PixelFormat.Format16bppRgb:
-                //case PixelFormat.Format24bppRgb:
-                //etc.
-                case PixelFormat.Format32bppRgb:
+                case PixelFormat.Format24bppRgb:
+                    cPtr = Marshal.AllocHGlobal(24);
+                    Marshal.WriteByte(cPtr, color.B);
+                    Marshal.WriteByte(cPtr + 1, color.G);
+                    Marshal.WriteByte(cPtr + 2, color.R);
+                    Marshal.WriteIntPtr(ptr + offset + x * 3, cPtr);
+                    Marshal.FreeHGlobal(cPtr);
+                    break;
+                case PixelFormat.Format32bppRgb:                
                 case PixelFormat.Format32bppArgb:
-                    Marshal.WriteIntPtr(ptr + offset * y + x * 4, cPtr);
+                    Marshal.WriteInt32(ptr + offset + x * 4, color.ToArgb());
                     break;
                 default:
                     throw new NotImplementedException();
