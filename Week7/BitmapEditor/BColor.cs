7acfc2f70535f8422b5c212d016ce35ef3d209b6
abatar1 <evgenyhalzov@gmail.com>
2016-11-11 16:41:25 +0600

    2 task 2.1 update

--- Week7/BitmapEditor/BColor.cs
+++ Week7/BitmapEditor/BColor.cs
@@ -8,12 +8,10 @@ namespace Week7
     public class BColor
     {
         public IntPtr Ptr { get; }
-        public int Size { get; }
-
-        public BColor(IntPtr cPtr, int cSize)
-        {
-            Ptr = cPtr;
-            Size = cSize;
+        private int size;
+        public int Size {
+            get { return size / 8; }
+            private set { size = value; }
         }
 
         public BColor(Color color, PixelFormat pixelFormat)
@@ -23,16 +21,16 @@ namespace Week7
                 //here comes cases with pixel formats
                 case PixelFormat.Format16bppGrayScale:
                     Ptr = new IntPtr((char)(0.299 * color.R + 0.587 * color.G + 0.114 * color.B));
-                    Size = 2;
+                    size = 16;
                     break;
                 case PixelFormat.Format24bppRgb:
                     Ptr = Marshal.UnsafeAddrOfPinnedArrayElement(new byte[] { color.B, color.G, color.R }, 0);
-                    Size = 3;
+                    size = 24;
                     break;
                 case PixelFormat.Format32bppRgb:
                 case PixelFormat.Format32bppArgb:
                     Ptr = new IntPtr(color.ToArgb());
-                    Size = 4;
+                    size = 32;
                     break;
                 default:
                     throw new NotImplementedException();
